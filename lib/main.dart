9a2e7d45285e27798233cbf63937890f316561cc
HenryBS18 <110516086+HenryBS18@users.noreply.github.com>
2023-10-24 22:34:55 +0800

    add stateful widget counter

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyStatelessApp());
+  // runApp(const MyStatefulWidgetApp());
 }
 
 class MyStatelessApp extends StatelessWidget {
@@ -30,12 +31,50 @@ class CounterWidget extends StatelessWidget {
           const Text('Counter Value: 0'),
           const SizedBox(height: 10),
           ElevatedButton(
-            onPressed: () {
-              // TODO: Increment counter
-            },
+            onPressed: () {},
             child: const Text('Increment'),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class MyStatefulWidgetApp extends StatefulWidget {
+  const MyStatefulWidgetApp({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyStatefulWidgetAppState createState() => _MyStatefulWidgetAppState();
+}
+
+class _MyStatefulWidgetAppState extends State<MyStatefulWidgetApp> {
+  int counter = 0;
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Stateful Counter App')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Counter Value: $counter'),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: incrementCounter,
+                child: const Text('Increment'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
